9c58bd500bb89c9d40f9ba2b21f9d2a77d62069b
wdmavenarius@gmail.com <>
2018-10-24 16:33:37 -0400

    Let God spare us

---
+++ src/letthiswork/LetThisWork.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package letthiswork;
+
+import java.util.Scanner;
+
+public class LetThisWork {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Hello there traveler, what is your name? ");
+    }
+    
+}
